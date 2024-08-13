6d36ef7112eca01fe9d17e34bed8ee35b88ee62d
Vera Freiman <fre-vera@yandex.ru>
2024-08-13 17:11:02 +0300

    Fixed

--- js/calcSum/calcSum.test.mjs
+++ js/calcSum/calcSum.test.mjs
@@ -3,6 +3,6 @@ import { calcSum } from './calcSum.mjs';
 try {
   console.log(calcSum(3, 3));
   console.log(calcSum(3));
-} catch (e) {
-  console.error(e.message);
+} catch (error) {
+  console.error(error);
 }
